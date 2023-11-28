fda8c5a719721dea2e3d6191f17bd1f8f6803fad
ZarcxCliff <113826447+ZarcxCliff@users.noreply.github.com>
2023-11-28 23:16:05 +0100

    Add files via upload

---
+++ rockpaper/index.js
@@ -0,0 +1,17 @@
+// const logout = document.getElementById("logout");
+// const playerSelection = "rock";
+// const computerSelection = "paper";
+// function playRound(playerSelection, computerSelection) {
+// 	if (playerSelection === "rock" && computerSelection === "paper") {
+// 		console.log("computer wins");
+// 	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+// 		console.log("player wins");
+// 	} else {
+// 		console.log("game has no winner");
+// 	}
+// }
+
+// let play = playRound(playerSelection, computerSelection);
+// console.log(play);
+// //   const computerSelection = getComputerChoice();
+// //   console.log(playRound(playerSelection, computerSelection));
